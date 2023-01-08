e72f30f4545cc764867ccca6908de3afb5dd3c89
WOLFIE_OG <35380754+WOLFIE-OG@users.noreply.github.com>
2023-01-08 13:57:16 +0000

    Update spacelaces.py

--- spacelaces.py
+++ spacelaces.py
@@ -2,46 +2,71 @@ from pythonosc.udp_client import SimpleUDPClient
 import time
 import os
 import ctypes
+import random
 
 sender = SimpleUDPClient("127.0.0.1", 9000)
 
-delay = 1.2
-text = "It's the mix king!   "
-text2 = "SPACE LACES----------"
-text7 = " <High Vaultage> "
-text8 = "soundcloud.com/space-laces"
+chars = ["▓", "▒", "░", "█"]
+
+delay = 1.6
+mix_king = "It's the mix king!   "
+space_laces = "SPACE LACES----------"
+high_vaultage = " <High Vaultage> "
+url = "soundcloud.com/space-laces"
 
 while True:
+    
+    inc = 0
+    bar = ""
+    sender.send_message("/chatbox/input", ("Loading RUN.dll", True))
+    ctypes.windll.kernel32.SetConsoleTitleW("Loading RUN.dll")
+    os.system("cls")
+    print("Loading RUN.dll")
+    time.sleep(delay)
+    
+    for i in range(0, 10):
+        bar += "▒"
+        inc += 10
+        sender.send_message("/chatbox/input", (f"{bar} {inc}%", True))
+        ctypes.windll.kernel32.SetConsoleTitleW("Loading RUN.dll")
+        os.system("cls")
+        print("Loading RUN.dll")
+        time.sleep(delay)
+
     _a = True
     for i in range(0, 10):
         if _a is True:
-            sender.send_message("/chatbox/input", (text8, True))
-            ctypes.windll.kernel32.SetConsoleTitleW(text8)
+            sender.send_message("/chatbox/input", (url, True))
+            ctypes.windll.kernel32.SetConsoleTitleW(url)
             os.system("cls")
-            print(text8)
+            print(url)
             _a = False
 
         else:
-            sender.send_message("/chatbox/input", ("_" * len(text8), True))
-            ctypes.windll.kernel32.SetConsoleTitleW("_" * len(text8))
+            _p = []
+            for i in range(0, int(len(url) / 2)):
+                _p.append(random.choice(chars))
+
+            sender.send_message("/chatbox/input", ("".join(_p), True))
+            ctypes.windll.kernel32.SetConsoleTitleW("".join(_p))
             os.system("cls")
-            print("_" * len(text8))
+            print("".join(_p))
             _a = True
 
         time.sleep(delay)
 
-    for i in range(0, round(len(text7) / 2)):
-        __ = text7[:-i][i:]
-        if __ != text7:
+    for i in range(0, round(len(high_vaultage) / 2)):
+        __ = high_vaultage[:-i][i:]
+        if __ != high_vaultage:
             sender.send_message("/chatbox/input", (__, True))
             ctypes.windll.kernel32.SetConsoleTitleW(__)
             os.system("cls")
             print(__)
             time.sleep(delay)
 
-    for i in range(0, len(text7)):
-        __ = text7[-i:]
-        if __ != text7:
+    for i in range(0, len(high_vaultage)):
+        __ = high_vaultage[-i:]
+        if __ != high_vaultage:
             sender.send_message("/chatbox/input", (__, True))
             ctypes.windll.kernel32.SetConsoleTitleW(__)
             os.system("cls")
@@ -49,54 +74,54 @@ while True:
             time.sleep(delay)
 
     for i in range(0, 10):
-        sender.send_message("/chatbox/input", (text, True))
-        ctypes.windll.kernel32.SetConsoleTitleW(text)
+        sender.send_message("/chatbox/input", (mix_king, True))
+        ctypes.windll.kernel32.SetConsoleTitleW(mix_king)
         os.system("cls")
-        print(text)
+        print(mix_king)
         for i in range(0, 4):
-            text = text[1:] + text[0]
+            mix_king = mix_king[1:] + mix_king[0]
         time.sleep(delay)
 
     for i in range(0, 2):
         for i in range(0, 5):
-            sender.send_message("/chatbox/input", (text2, True))
-            ctypes.windll.kernel32.SetConsoleTitleW(text2)
+            sender.send_message("/chatbox/input", (space_laces, True))
+            ctypes.windll.kernel32.SetConsoleTitleW(space_laces)
             os.system("cls")
-            print(text2)
-            text2 = text2[:-2]
-            text2 = "-" + text2
-            text2 = "-" + text2
+            print(space_laces)
+            space_laces = space_laces[:-2]
+            space_laces = "-" + space_laces
+            space_laces = "-" + space_laces
             time.sleep(delay)
 
         for i in range(0, 5):
-            sender.send_message("/chatbox/input", (text2, True))
-            ctypes.windll.kernel32.SetConsoleTitleW(text2)
+            sender.send_message("/chatbox/input", (space_laces, True))
+            ctypes.windll.kernel32.SetConsoleTitleW(space_laces)
             os.system("cls")
-            print(text2)
-            text2 = text2[2:]
-            text2 = text2 + "-"
-            text2 = text2 + "-"
+            print(space_laces)
+            space_laces = space_laces[2:]
+            space_laces = space_laces + "-"
+            space_laces = space_laces + "-"
             time.sleep(delay)
 
-    sender.send_message("/chatbox/input", (text2, True))
-    ctypes.windll.kernel32.SetConsoleTitleW(text2)
+    sender.send_message("/chatbox/input", (space_laces, True))
+    ctypes.windll.kernel32.SetConsoleTitleW(space_laces)
     os.system("cls")
-    print(text2)
+    print(space_laces)
     time.sleep(delay)
 
     for i in range(0, 4):
         i += 1
         if i == 4:
-            text3 = "VAULTAGE ???"
+            vautlage = "VAULTAGE ???"
         else:
-            text3 = f"VAULTAGE 00{i}"
-        sender.send_message("/chatbox/input", (text3, True))
-        ctypes.windll.kernel32.SetConsoleTitleW(text3)
+            vautlage = f"VAULTAGE 00{i}"
+        sender.send_message("/chatbox/input", (vautlage, True))
+        ctypes.windll.kernel32.SetConsoleTitleW(vautlage)
         os.system("cls")
-        print(text3)
+        print(vautlage)
         time.sleep(delay)
 
-    text5 = [
+    lyrics_vautlage = [
         "He's been away a long time...",
         "Once a man,",
         "he is now something more than human...",
@@ -108,14 +133,14 @@ while True:
         "to destroy!",
     ]
 
-    for i in text5:
+    for i in lyrics_vautlage:
         sender.send_message("/chatbox/input", (i, True))
         ctypes.windll.kernel32.SetConsoleTitleW(i)
         os.system("cls")
         print(i)
         time.sleep(delay)
 
-    text6 = [
+    random_quotes = [
         "I got choppaz in the trunk",
         "Gimme some room, I'm throwing elbows",
         "Let's get ready to rumble!",
@@ -128,7 +153,7 @@ while True:
         "Chika-chika",
         "<SPACE LACES>",
     ]
-    for i in text6:
+    for i in random_quotes:
         sender.send_message("/chatbox/input", (i, True))
         ctypes.windll.kernel32.SetConsoleTitleW(i)
         os.system("cls")
